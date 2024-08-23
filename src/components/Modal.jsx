53d747b47a3933e9611a627a39750d3eb9c4773d
Sailesh3000 <chandrasailesh30@gmail.com>
2024-08-23 11:15:00 +0530

    changed heading

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,7 +5,7 @@ const Modal = ({ closeModal }) => {
     <div className="fixed inset-0 flex items-center justify-center z-50">
       <div className="absolute inset-0 bg-black opacity-50"></div>
       <div className="bg-[#393E46] p-8 rounded-lg shadow-lg z-10">
-        <h3 className="text-2xl text-[#FFD369] tracking-tight font-extrabold mb-4">Sign Up to View More</h3>
+        <h3 className="text-2xl text-[#FFD369] tracking-tight font-extrabold mb-4">Sign Up to View Rentals</h3>
         <p className="mb-6 text-white">Please sign up or log in to access detailed information about this book.</p>
         <div className="flex justify-end space-x-4">
           <button
