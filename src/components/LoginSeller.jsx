949a6cb76781d154eb37b1e51485aecb4e9ede6b
Sailesh3000 <chandrasailesh30@gmail.com>
2024-09-13 18:04:47 +0530

    added seller dashboard

--- src/components/LoginSeller.jsx
+++ src/components/LoginSeller.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Login = () => {
+const LoginSeller = ({ onToggle }) => {
   return (
     <div className="justify-center px-4 lg:py-0 w-[500px] sm:px-8 sm:gap-2">
       <div className="w-full bg-[#393E46] rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1 className="text-2xl text-[#FFD369] tracking-tight font-extrabold mb-4">
-            Create a Seller account
+            Login to your account
           </h1>
           <form className="space-y-4 md:space-y-6" action="#">
             <div>
@@ -31,40 +31,13 @@ const Login = () => {
                 required 
               />
             </div>
-            <div>
-              <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-white">Confirm password</label>
-              <input 
-                type="password" 
-                name="confirm-password" 
-                id="confirm-password" 
-                placeholder="••••••••" 
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#FFD369] focus:border-[#FFD369] block w-full p-2.5" 
-                required 
-              />
-            </div>
-            <div className="flex items-start">
-              <div className="flex items-center h-5">
-                <input 
-                  id="terms" 
-                  aria-describedby="terms" 
-                  type="checkbox" 
-                  className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-[#FFD369]" 
-                  required 
-                />
-              </div>
-              <div className="ml-3 text-sm">
-                <label htmlFor="terms" className="font-light text-white">
-                  I accept the <a className="font-medium text-[#FFD369] hover:underline" href="#">Terms and Conditions</a>
-                </label>
-              </div>
-            </div>
             <button 
               type="submit" 
               className="w-full text-gray-900 bg-[#FFD369] hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-[#FFD369] font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-              Create an account
+              Login
             </button>
             <p className="text-sm font-light text-white">
-              Already have an account? <a href="#" className="font-medium text-[#FFD369] hover:underline">Login here</a>
+              Don't have an account? <a href="#" onClick={onToggle} className="font-medium text-[#FFD369] hover:underline">Sign up here</a>
             </p>
           </form>
         </div>
@@ -73,4 +46,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default LoginSeller;
---
+++ src/components/SignupSeller.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const SignupSeller = ({ onToggle }) => {
+  return (
+    <div className="justify-center px-4 lg:py-0 w-[500px] sm:px-8 sm:gap-2">
+      <div className="w-full bg-[#393E46] rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <h1 className="text-2xl text-[#FFD369] tracking-tight font-extrabold mb-4">
+            Create a Seller account
+          </h1>
+          <form className="space-y-4 md:space-y-6" action="#">
+            <div>
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">Your email</label>
+              <input 
+                type="email" 
+                name="email" 
+                id="email" 
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#FFD369] focus:border-[#FFD369] block w-full p-2.5" 
+                placeholder="name@company.com" 
+                required 
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="block mb-2 text-sm font-medium text-white">Password</label>
+              <input 
+                type="password" 
+                name="password" 
+                id="password" 
+                placeholder="••••••••" 
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#FFD369] focus:border-[#FFD369] block w-full p-2.5" 
+                required 
+              />
+            </div>
+            <div>
+              <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-white">Confirm password</label>
+              <input 
+                type="password" 
+                name="confirm-password" 
+                id="confirm-password" 
+                placeholder="••••••••" 
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#FFD369] focus:border-[#FFD369] block w-full p-2.5" 
+                required 
+              />
+            </div>
+            <div className="flex items-start">
+              <div className="flex items-center h-5">
+                <input 
+                  id="terms" 
+                  aria-describedby="terms" 
+                  type="checkbox" 
+                  className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-[#FFD369]" 
+                  required 
+                />
+              </div>
+              <div className="ml-3 text-sm">
+                <label htmlFor="terms" className="font-light text-white">
+                  I accept the <a className="font-medium text-[#FFD369] hover:underline" href="#">Terms and Conditions</a>
+                </label>
+              </div>
+            </div>
+            <button 
+              type="submit" 
+              className="w-full text-gray-900 bg-[#FFD369] hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-[#FFD369] font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+              Create an account
+            </button>
+            <p className="text-sm font-light text-white">
+              Already have an account? <a href="#" onClick={onToggle} className="font-medium text-[#FFD369] hover:underline">Login here</a>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignupSeller;
