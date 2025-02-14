8d994bd8f8c2a30fbe250d61814aaffd512dcf5f
Sailesh3000 <chandrasailesh30@gmail.com>
2025-02-14 20:47:42 +0530

    changed backend url

--- src/components/SignupSeller.jsx
+++ src/components/SignupSeller.jsx
@@ -77,7 +77,7 @@ const SignupSeller = ({ onToggle }) => {
 
     try {
       setLoading(true);
-      const response = await fetch('https://shelfx-backend.netlify.app/SignupSeller', {
+      const response = await fetch('https://shelf-x-backend.vercel.app/SignupSeller', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
